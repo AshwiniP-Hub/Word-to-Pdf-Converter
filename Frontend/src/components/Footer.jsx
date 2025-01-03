e32301af045ceecbec5682966417b3679f307e53
AshwiniP-Hub <ashwinivpatil4@gmail.com>
2025-01-03 12:22:03 +0530

    First Commit
    Project completed

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="item-center justify-center">
+        <hr className='shadow-lg' />
+        <h1 className='text-center py-3 text-sm'>
+            Copyright @2024 - All right reserved by Ashwini Patil.
+        </h1>
+    </div>
+    </>
+  )
+}
+
+export default Footer
